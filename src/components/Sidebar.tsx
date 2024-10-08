c560e03453e4987390cb8e92a1807c899612b722
sanjeev0303 <sanjeev.081021@gmail.com>
2024-10-08 07:01:02 +0530

    adding sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
 
-export default Sidebar
+  const sidebarClassname = `fixed flex flex-col h-screen justify-between shadow-xl transition-all duration-300 z-20 dark:bg-black overflow-y-auto bg-white w-64`;
+
+  return (
+    <div className={sidebarClassname}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* Top Logo */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            EDLIST
+          </div>
+        </div>
+
+        {/* Team */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="logo" width={40} height={40} />
+
+          <div className="flex flex-col">
+          <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+            EDROH TEAM
+          </h3>
+
+          <div className="mt-1 flex items-start gap-2">
+            <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dart:text-gray-400" />
+            <p className="text-xs text-gray-500">Private</p>
+          </div>
+          </div>
+        </div>
+
+        {/* NAVBAR LINKS */}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
